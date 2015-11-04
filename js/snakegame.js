c8f18b0554fdf120d57c6c0e24d8398d89bbd9d3
Ching Yang <Ching@Chings-MacBook-Air.local>
2015-11-04 17:35:26 +0800

    completed Two Players

--- js/snakegame.js
+++ js/snakegame.js
@@ -1,16 +1,25 @@
-  
-
+// SW is snake width
 var SW = 5;
+var snackX;
+var snackY;
+// Setup player1 variables
 var snakeLengthOne = 10;
 var scoreOne;
 var curXOne;
 var curYOne;
-var snackX;
-var snackY;
 var xArrOne = [];
 var yArrOne = [];
 var inpOne = "r";
 var curDirOne = "r";
+// Setup player2 variables
+var snakeLengthTwo = 10;
+var scoreTwo;
+var curXTwo;
+var curYTwo;
+var xArrTwo = [];
+var yArrTwo = [];
+var inpTwo = "r";
+var curDirTwo = "r";
 var speed = 100;
 
 $(document).ready(function(){
@@ -20,7 +29,7 @@ $(document).ready(function(){
     value: 50,
     max: 100,
     min: 0,
-    step: 20,
+    step: 25,
     slide: function(event, ui) {
       if (ui.value > 70) {
         $("#speed-limit").text("Fast");
@@ -34,64 +43,83 @@ $(document).ready(function(){
   });
 
   $(document).keydown(function(e){
-    if (curDirOne == "l") {
+    if (curDirOne == "l" || curDirOne == "r") {
       if (e.keyCode==38) {
         inpOne = "u";
       } else if (e.keyCode==40) {
         inpOne = "d";
       }
-    } else if (curDirOne == "u") {
-      if (e.keyCode==37) {
-        inpOne = "l";
-      } else if (e.keyCode==39) {
-        inpOne = "r";
-      }
-    } else if (curDirOne == "r") {
-      if (e.keyCode==38) {
-        inpOne = "u";
-      } else if (e.keyCode==40) {
-        inpOne = "d";
-      }
-    } else if (curDirOne == "d") {
+    } else if (curDirOne == "u" || curDirOne == "d") {
       if (e.keyCode==37) {
         inpOne = "l";
       } else if (e.keyCode==39) {
         inpOne = "r";
       }
     }
-});
+  });
+
+  $(document).keydown(function(e){
+    if (curDirTwo == "l" || curDirTwo == "r") {
+      if (e.keyCode==87) {
+        inpTwo = "u";
+      } else if (e.keyCode==83) {
+        inpTwo = "d";
+      }
+    } else if (curDirTwo == "u" || curDirTwo == "d") {
+      if (e.keyCode==65) {
+        inpTwo = "l";
+      } else if (e.keyCode==68) {
+        inpTwo = "r";
+      }
+    }
+  });
 
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
   var w=$('#canvas').width();
   var h=$('#canvas').height();
 
-  function createSnack(snakeLength) {
+  function createSnack(player) {
     snackX = Math.floor(((w/SW)-1)*Math.random())*SW;
     snackY = Math.floor(((h/SW)-1)*Math.random())*SW;
-    var match = false;
-    for(var i = 0; i < snakeLength;i++) {
+    for(var i = 0; i < snakeLengthOne;i++) {
       if((snackX == xArrOne[i] && snackY == yArrOne[i])) {
-        match = true;
+        createSnack(1);
       }
     }
-    if (match == true) {
-      createSnack();
-    } else {
-      ctx.fillStyle = "green";
-      ctx.fillRect(snackX, snackY,SW,SW);
+    if (player == 2) {
+      for(var x = 0; x < snakeLengthTwo;x++) {
+        if((snackX == xArrTwo[i] && snackY == yArrTwo[i])) {
+        createSnack(2);
+        }
+      }
     }
+    ctx.fillStyle = "green";
+    ctx.fillRect(snackX, snackY,SW,SW);
   }
 
-  function initSnake(snakeLength,xArr,yArr,curX) {
-    for (var i = 0; i < snakeLength; i++) {
-      ctx.fillStyle = "black";
-      ctx.fillRect(i*5,0,SW,SW);
-      xArr.unshift(i*5);
-      yArr.unshift(0);
+
+  function initSnake(player,snakeLength,xArr,yArr,curX,curY) {
+    if (player == 1) {
+      for (var i = 0; i < snakeLength; i++) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(i*5,0,SW,SW);
+        xArr.unshift(i*5);
+        yArr.unshift(0);
+      }
+      window[curX] = (snakeLength - 1) * 5;
+      window[curY] = 0;
+    } else if (player == 2) {
+      for (var i = 0; i < snakeLength; i++) {
+        ctx.fillStyle = "blue";
+        ctx.fillRect(i*5,w-5,SW,SW);
+        xArr.unshift(i*5);
+        yArr.unshift(w-5);
+      }
+      window[curX] = (snakeLength - 1) * 5;
+      window[curY] = w - 5;
     }
-    window[curX] = (snakeLength - 1) * 5;
-    $("h5").text("Score: "+ scoreOne);
+    // $("h5").text("Score: "+ scoreOne);
   }
 
   function onePlay () {
@@ -108,22 +136,99 @@ $(document).ready(function(){
         scoreOne++;
         xArrOne.push(1);
         yArrOne.push(1);
-        moveSnake(snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
+        moveSnake(1,snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
         $("h5").text("Score: "+ scoreOne);
         createSnack(snakeLengthOne);
       }
       else {
-        moveSnake(snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
+        moveSnake(1,snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
       }
     } else {
       clearInterval(timer);
       ctx.font = "30px Arial";
       ctx.strokeText("GAME OVER",10,100);
-      $("#start-button").prop('disabled', false);
+      $("#onePlayer-button").prop('disabled', false);
+      $("#twoPlayer-button").prop('disabled', false);
     }
   }
 
-  function moveSnake(snakeLength,inp,curX,curY,curDir,xArr,yArr) {
+  function twoPlay () {
+    var gameOn = true;
+    var pOne = true;
+    var pTwo = true;
+    for(var x = 1; x<snakeLengthOne;x++) {
+      if (curXOne == xArrOne[x] && curYOne == yArrOne[x]) {
+        gameOn = false;
+        pOne = false;
+      } else if (curXTwo == xArrOne[x] && curYTwo == yArrOne[x]) {
+        gameOn = false;
+        pTwo = false;
+      }
+    } 
+    for(var y = 1; y<snakeLengthTwo;y++) {
+      if (curXTwo == xArrTwo[y] && curYTwo == yArrTwo[y]) {
+        gameOn = false;
+        pTwo = false;
+      } else if (curXOne == xArrTwo[y] && curYOne == yArrTwo[y]) {
+        gameOn = false;
+        pOne = false;
+      } 
+    }
+    if ((curXOne > w - SW || curYOne > h - SW || curXOne < 0 || curYOne < 0) && (curXTwo > w - SW || curYTwo > h - SW || curXTwo < 0 || curYTwo < 0)) {
+      gameOn = false;
+      pOne = false;
+      pTwo = false;
+    } else if (curXOne > w - SW || curYOne > h - SW || curXOne < 0 || curYOne < 0) {
+      gameOn = false;
+      pOne = false;
+    } else if (curXTwo > w - SW || curYTwo > h - SW || curXTwo < 0 || curYTwo < 0) {
+      gameOn = false;
+      pTwo = false;
+    }
+
+    if(gameOn) {
+      if (curXOne == snackX && curYOne == snackY) {
+        snakeLengthOne++;
+        scoreOne++;
+        xArrOne.push(1);
+        yArrOne.push(1);
+        moveSnake(1,snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
+        moveSnake(2,snakeLengthTwo,inpTwo,"curXTwo","curYTwo","curDirTwo",xArrTwo,yArrTwo);
+        $("h5").text("P1 Score: "+ scoreOne + "P2 Score: "+ scoreTwo);
+        createSnack(2);
+      } else if (curXTwo == snackX && curYTwo == snackY) {
+        snakeLengthTwo++;
+        scoreTwo++;
+        xArrTwo.push(1);
+        yArrTwo.push(1);
+        moveSnake(1,snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
+        moveSnake(2,snakeLengthTwo,inpTwo,"curXTwo","curYTwo","curDirTwo",xArrTwo,yArrTwo);
+        $("h5").text("P1 Score: "+ scoreOne + "P2 Score: "+ scoreTwo);
+        createSnack(2);
+      }
+      else {
+        moveSnake(1,snakeLengthOne,inpOne,"curXOne","curYOne","curDirOne",xArrOne,yArrOne);
+        moveSnake(2,snakeLengthTwo,inpTwo,"curXTwo","curYTwo","curDirTwo",xArrTwo,yArrTwo);
+      }
+    } else {
+      clearInterval(timer);
+      $("#start-button").prop('disabled', false);
+      if (!pOne && !pTwo) {
+        ctx.font = "30px Arial";
+        ctx.strokeText("DRAW",70,100);
+      } else if (!pOne) {
+        ctx.font = "30px Arial";
+        ctx.strokeText("Player 2 Wins",8,100);
+      } else if (!pTwo) {
+        ctx.font = "30px Arial";
+        ctx.strokeText("Player 1 Wins",8,100);
+      }
+      $("#onePlayer-button").prop('disabled', false);
+      $("#twoPlayer-button").prop('disabled', false);
+    }
+  }
+
+  function moveSnake(player,snakeLength,inp,curX,curY,curDir,xArr,yArr) {
     ctx.clearRect(xArr[snakeLength-1], yArr[snakeLength-1], SW, SW);
     if (inp == "l") {
       window[curX] -= SW;
@@ -142,12 +247,16 @@ $(document).ready(function(){
     yArr.unshift(window[curY]);
     xArr.pop();
     yArr.pop();
-    createSnake(snakeLength,xArr,yArr);
+    createSnake(player,snakeLength,xArr,yArr);
   }
 
-  function createSnake(snakeLength,xArr,yArr) {
+  function createSnake(player,snakeLength,xArr,yArr) {
     for (var i = 0; i < snakeLength; i++) {
-      ctx.fillStyle = "black";
+      if (player == 1) {
+        ctx.fillStyle = "black";
+      } else if (player == 2) {
+        ctx.fillStyle = "blue";
+      }
       ctx.fillRect(xArr[i],yArr[i],SW,SW);
     }
   }
@@ -156,7 +265,7 @@ $(document).ready(function(){
     
   }
 
-  $(document).on('click','#start-button', function() {
+  $(document).on('click','#onePlayer-button', function() {
     inpOne = "r";
     curDirOne = "r";
     scoreOne = 0;
@@ -166,11 +275,41 @@ $(document).ready(function(){
     curXOne = 0;
     curYOne = 0;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    initSnake(snakeLengthOne,xArrOne,yArrOne,"curXOne");
-    createSnack(snakeLengthOne);
-    $("#start-button").prop('disabled', true);
-    
+    $("h5").text("P1 Score: "+ scoreOne);
+    initSnake(1,snakeLengthOne,xArrOne,yArrOne,"curXOne","curYOne");
+    createSnack(1);
+    $("#onePlayer-button").prop('disabled', true);
+    $("#twoPlayer-button").prop('disabled', true);
     timer = setInterval(onePlay, speed);
+  });
+
+  $(document).on('click','#twoPlayer-button', function() {
+    // reset player 1 var
+    inpOne = "r";
+    curDirOne = "r";
+    scoreOne = 0;
+    snakeLengthOne = 10;
+    xArrOne = [];
+    yArrOne = [];
+    curXOne = 0;
+    curYOne = 0;
+    // reset player 2 var
+    snakeLengthTwo = 10;
+    scoreTwo = 0;
+    curXTwo = 0;
+    curYTwo = 0;
+    xArrTwo = [];
+    yArrTwo = [];
+    inpTwo = "r";
+    curDirTwo = "r";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    $("h5").text("P1 Score: "+ scoreOne + "P2 Score: "+ scoreTwo);
+    initSnake(1,snakeLengthOne,xArrOne,yArrOne,"curXOne","curYOne");
+    initSnake(2,snakeLengthTwo,xArrTwo,yArrTwo,"curXTwo","curYTwo");
+    createSnack(2);
+    $("#onePlayer-button").prop('disabled', true);
+    $("#twoPlayer-button").prop('disabled', true);
+    timer = setInterval(twoPlay, speed);
   });
 
 });
